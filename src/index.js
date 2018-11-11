0b8ea5b21fc0acc607de7ea4045984c93067dfa6
Erin Hinson <erinhinson@Erins-MacBook-Pro.local>
2018-11-11 12:26:09 -0800

    tweak

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import dndApp from './state/reducers';
 import App from './views/App';
 
 const store = createStore(
-    dndApp,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  dndApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'),
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
 );
