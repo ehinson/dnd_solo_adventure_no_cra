ee7c4b79764a03350e0a5e5235fdfdbf7725a6cb
Erin Hinson <erinhinson@Erins-MacBook-Pro.local>
2018-11-11 10:32:51 -0800

    More config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        modules: ['node_modules'],
+    },
     module: {
         rules: [
             {
@@ -26,4 +36,10 @@ module.exports = {
             filename: './index.html',
         }),
     ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        historyApiFallback: true,
+    },
 };
